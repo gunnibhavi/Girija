1bad9519881e6a06b0e7965e44c854c332d36ceb
gunnibhavi <gunnibhavi@gmail.com>
2021-03-13 18:22:29 +0530

    Programs added

---
+++ MyBio.java
@@ -0,0 +1,20 @@
+/**
+*Name:Girija Unnibhavi
+*Description:java program to display biodata of ours
+*Date: 3/13/2021
+*/
+class MyBio{
+            static public void main(String args[]){
+				int personalId=1001;
+				String personName="Girija Unnibhavi";
+				String location="Nidagundi";
+				String contactNumber="7483931722";
+				String email="gunnibhavi@gmail.com";
+				System.out.println("PersonalId:"+personalId);
+			System.out.println("Name:"+personName);
+			System.out.println("Location:"+location);
+			System.out.println("Contact:"+contactNumber);
+			System.out.println("Email:"+email );
+			}
+			}
+			
